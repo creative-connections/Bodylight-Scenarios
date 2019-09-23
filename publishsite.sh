a23f77f2252ec445f89cf99a9088676c6cf5d223
Tomas Kulhanek <tmkulhanek@gmail.com>
2019-09-23 09:36:09 +0100

    update git gh-pages and ignore build

--- publishsite.sh
+++ publishsite.sh
@@ -1,1 +1,5 @@
-git subtree push --prefix build/virtualbody origin gh-pages
+#git subtree push --prefix build/virtualbody origin gh-pages
+#using https://github.com/X1011/git-directory-deploy
+export GIT_DEPLOY_DIR=build/virtualbody
+export GIT_DEPLOY_REPO=https://github.com/creative-connections/Bodylight-Scenarios.git
+./deploy.sh
