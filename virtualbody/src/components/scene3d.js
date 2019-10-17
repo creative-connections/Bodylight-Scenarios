ef8ca38d1ce80e2ae8c9c4b517cb52a3054e39c2
Tomas Kulhanek <tmkulhanek@gmail.com>
2019-10-17 17:04:30 +0100

    UPDATE: animate skin,skeleton opacity, blink

--- virtualbody/src/components/scene3d.js
+++ virtualbody/src/components/scene3d.js
@@ -26,6 +26,9 @@ export class Scene3d {
       this.renderer.setSize( w, h );
     };
     //this.controls.handleResize()
+    this.skinstate = 0;
+    this.skeletonstate = 0;
+    this.eyestate = 0;
   }
 
   attached() {
@@ -56,7 +59,10 @@ export class Scene3d {
     window.that.scene.add(window.that.camera);
 
 
-    this.loadobjects([objects3d.skinskeleton]);
+    this.loadobjects([objects3d.skinskeleton])
+      .then(x=>{
+        //this.scheduleblink();
+      })
     //,objects3d.lungs,objects3d.arteries,objects3d.veins
     //TODO add other objects
     this.renderer.gammaOutput = true;
@@ -67,6 +73,12 @@ export class Scene3d {
 
     window.addEventListener( 'resize', this.onWindowResize, false );
     this.animate();
+    //setInterval(this.blink,200)
+    //this.scheduleblink();
+  }
+
+  dettached() {
+    this.stopblink();
   }
 
   declareOrbitControls() {
@@ -80,7 +92,7 @@ export class Scene3d {
   loadobjects(names) {
     this.index = 0;
     this.names = names;
-    this.loadobjects2(names);
+    return this.loadobjects2(names);
   }
 
   loadobjects2() {
@@ -89,7 +101,7 @@ export class Scene3d {
       let localurl = objects3d.local + this.names[this.index];
       let remoteurl = objects3d.remote + this.names[this.index];
       this.index++;
-      this.httpclient.fetch(localurl, {method: 'head'})
+      return this.httpclient.fetch(localurl, {method: 'head'})
         .then(response => {
         //console.log(response)
           if (response.status === 404) this.loadfromurl(remoteurl);
@@ -123,6 +135,7 @@ export class Scene3d {
         console.log( 'An error happened', error );
         window.that.progresstxt += 'An error happened ' + error;
       });
+
   }
 
   animate() {
@@ -131,26 +144,106 @@ export class Scene3d {
     window.requestAnimationFrame( window.that.animate );
     window.that.renderer.render( window.that.scene, window.that.camera );
     //window.that.stats.update()
+    //setInterval()
+    //
   }
 
   switchskin() {
     let skin = this.scene.getObjectByName('Skin', true);
     let apron = this.scene.getObjectByName('Apron', true);
-    skin.visible = ! skin.visible;
-    apron.visible = ! apron.visible;
+    //    console.log('skin:', skin);
+    window.that = this;
+    if (this.skinstate === 0) {this.skinstate++; this.opaquefrom100to50(skin, apron); } else if (this.skinstate === 1) {this.skinstate++; this.opaquefrom50to0(skin, apron); } else if (this.skinstate === 2) {this.skinstate++; this.opaquefrom0to50(skin, apron); } else if (this.skinstate === 3) {this.skinstate = 0; this.opaquefrom50to100(skin, apron); }
     //console.log('switchskin:', skin.visible);
+  }
+
+  opaquefrom100to50(o1, o2) {
+    o1.material.transparent = 1.0; o1.material.opacity = 1.0;
+    o2.material.transparent = 1.0; o2.material.opacity = 1.0;
+    if (this.timerid) clearInterval(this.timerid);
+    this.timerid = setInterval(this.decOp, 50, o1, o2, 0.02, 0.5);
+
+    //console.log('opaque timerid',this.timerid);
+  }
+
+  decOp(o1, o2, decr, min) {
+    o1.material.opacity -= decr;
+    o2.material.opacity -= decr;
+    console.log('decop', o1.material.opacity);
+    console.log('decop min', min);
+    console.log('decop timerid', this.timerid);
+    if (o1.material.opacity <= min) {clearInterval(that.timerid); that.timerid = false;}
+  }
+
+  opaquefrom50to0(o1, o2) {
+    o1.material.transparent = 1.0; o1.material.opacity = 0.5;
+    o2.material.transparent = 1.0; o2.material.opacity = 0.5;
+    if (this.timerid) clearInterval(this.timerid);
+
+    this.timerid = setInterval(this.decOp, 50, o1, o2, 0.02, 0);
+  }
+  opaquefrom0to50(o1, o2) {
+    o1.material.transparent = 1.0; o1.material.opacity = 0;
+    o2.material.transparent = 1.0; o2.material.opacity = 0;
+    if (this.timerid) clearInterval(this.timerid);
+    this.timerid = setInterval(this.incOp, 50, o1, o2, 0.02, 0.5);
+  }
+  opaquefrom50to100(o1, o2) {
+    o1.material.transparent = 1.0; o1.material.opacity = 0.5;
+    o2.material.transparent = 1.0; o2.material.opacity = 0.5;
+    if (this.timerid) clearInterval(this.timerid);
+    this.timerid = setInterval(this.incOp, 50, o1, o2, 0.02, 1);
+  }
+
+  incOp(o1, o2, incr, max) {
+    o1.material.opacity += incr;
+    o2.material.opacity += incr;
+    console.log('incop', o1.material.opacity);
+    if (o1.material.opacity >= max) {clearInterval(that.timerid); that.timerid = false;}
   }
 
   switchskeleton() {
     let skeleton = this.scene.getObjectByName('Skeleton', true);
     let eyes = this.scene.getObjectByName('Eyes', true);
-    skeleton.visible = ! skeleton.visible;
-    eyes.visible = ! eyes.visible;
+    //skeleton.visible = ! skeleton.visible;
+    //eyes.visible = ! eyes.visible;
+    if (this.skeletonstate === 0) {this.skeletonstate++; this.opaquefrom100to50(skeleton, eyes); } else if (this.skeletonstate === 1) {this.skeletonstate++; this.opaquefrom50to0(skeleton, eyes);} else if (this.skeletonstate === 2) {this.skeletonstate++; this.opaquefrom0to50(skeleton, eyes); } else if (this.skeletonstate === 3) {this.skeletonstate = 0; this.opaquefrom50to100(skeleton, eyes); }
   }
 
   blink() {
     let eyes = this.scene.getObjectByName('Eyes', true);
-    eyes.castShadow = ! eyes.castShadow;
-    console.log('eyes', eyes);
+    this.blinke(eyes);
+    //this.scheduleblink();
+  }
+
+  scheduleblink() {
+    let eyes = this.scene.getObjectByName('Eyes', true);
+    window.that = this;
+    this.blinkid = setInterval(this.blinke, 5000, eyes);
+  }
+
+  stopblink() {
+    clearInterval(this.blinkid);
+  }
+
+  blinke(eyes) {
+    let tempmap = that.switchmapcolor(eyes, {r: 0.6, g: 0.3, b: 0.3});
+    setTimeout(that.switchcolormap, 200, eyes, {r: 1, g: 1, b: 1}, tempmap);
+  }
+
+  switchmapcolor(o1, c1) {
+    let tmp = o1.material.map;
+    o1.material.map = null;
+    o1.material.color = c1;
+    o1.material.needsUpdate = true;
+    return tmp;
+  }
+
+  switchcolormap(o1, c1, map) {
+    o1.material.map = map;
+    //o1.material.map=null;
+    o1.material.color = c1;
+    o1.material.needsUpdate = true;
+    //return tmp;
   }
 }
